cdd5ff39cee9dff6d360fe6e0be279e1b9890332
yuki hara <hara.0509@icloud.com>
2021-02-05 21:21:11 +0900

    first commit

---
+++ src/components/ColorfulMsg.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ColorfulMsg = (props) =>{
+    return(
+    <p style={{ color: props.color }}>{props.children}</p>
+    );
+}
+
+export default ColorfulMsg;
---
+++ src/components/Msg.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Msg = ()=>{
+    return <p style={{color:'blue'}}>どうだ！</p>;
+}
+
+export default Msg;
